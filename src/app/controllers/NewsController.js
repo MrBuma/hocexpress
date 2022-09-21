d277b2e2727eede9dacd2b6525207bba817ecf2a
Kiet Bui <hung.buma@gmail.com>
2022-09-21 15:58:11 +0700

    create news site route

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+
+
+class NewsController {
+    //[GET] /new
+    index(req, res){
+        res.render('news');
+    }
+
+}
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const { engine } = require('express-handlebars');
 const path = require('path');
 require('dotenv').config();
 const morgan = require('morgan');
+const route = require('./routers');
 const app = express();
-
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 //template Engine
 app.engine(
     'hbs',
@@ -19,14 +23,20 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('combined'));
-console.log(__dirname);
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.render('home');
-});
 
+route(app);
+// app.get('/', (req, res) =>{
+//     res.render('home');
+// });
+// app.get('/search', (req, res) =>{
+//     res.render('search');
+// });
+// app.post('/search', (req, res) =>{
+//     res.render('search');
+// });
 
 app.listen(port, (req, res)=>{
     console.log(` Server is running at port ${port}`);
